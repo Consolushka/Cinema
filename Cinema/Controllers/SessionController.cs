1860523829bece326142f901e3d7903520540656
Kerby-vlad <53996780+Kerby-vlad@users.noreply.github.com>
2021-03-20 22:06:18 +0300

    20.03.2021   removed useless controller and views   refactored sessions

--- Cinema/Controllers/SessionController.cs
+++ Cinema/Controllers/SessionController.cs
@@ -35,17 +35,16 @@ namespace Cinema.Controllers
                 return NotFound();
             }
 
-            ViewBag.Movie = _context.Movie.SingleOrDefault(m => m.Id == session.MovieId);
-            ViewBag.Hall = _context.Hall.SingleOrDefault(h => h.Id == session.HallId);
-            ViewBag.Seats = GetFullSessionSeats();
-            return View(session);
+            UserSession userSession = new UserSession(session, _context);
+
+            return View(userSession);
         }
 
         public void CreateReservation()
         {
             StreamReader sr = new StreamReader(Request.Body);
             UserReservation data = JsonSerializer.Deserialize<UserReservation>(sr.ReadToEnd());
-            foreach(int seatId in data.ReservedSeats)
+            foreach (int seatId in data.ReservedSeats)
             {
                 Reservation res = new Reservation();
                 res.SeatId = seatId;
@@ -55,55 +54,6 @@ namespace Cinema.Controllers
                 _context.Reservation.Add(res);
             }
             _context.SaveChanges();
-        }
-
-        private Dictionary<int, List<SessionSeat>> GetFullSessionSeats()
-        {
-            List<SessionSeat> seatsList = new List<SessionSeat>();
-
-            foreach (Seat seat in _context.Seat.ToList())
-            {
-                seatsList.Add(CreateSessionSeat(seat));
-            }
-
-            return GetFormatedSeatList(seatsList);
-
-        }
-
-        private SessionSeat CreateSessionSeat(Seat seat)
-        {
-            SessionSeat sessionSeat = new SessionSeat(seat, session,_context);
-
-            return sessionSeat;
-        }
-
-        private Dictionary<int, List<SessionSeat>> GetFormatedSeatList(List<SessionSeat> unformatted)
-        {
-            List<SessionSeat> listByRowsAndNumber = new List<SessionSeat>();
-
-            listByRowsAndNumber = unformatted.OrderBy(x => x.Row).ThenBy(x => x.Number).ToList();
-
-            var formattedList = GetSeatsByRows(listByRowsAndNumber);
-
-            return formattedList;
-        }
-
-        private Dictionary<int, List<SessionSeat>> GetSeatsByRows(List<SessionSeat> seatsList)
-        {
-            Dictionary<int, List<SessionSeat>> seatsByRow = new Dictionary<int, List<SessionSeat>>();
-            foreach (SessionSeat seat in seatsList)
-            {
-                if (seatsByRow.ContainsKey(seat.Row))
-                {
-                    seatsByRow[seat.Row].Add(seat);
-                }
-                else
-                {
-                    seatsByRow.Add(seat.Row, new List<SessionSeat> { seat });
-                }
-            }
-
-            return seatsByRow;
         }
     }
 }
---
+++ Cinema/Models/UserSession.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cinema.Data;
+
+namespace Cinema.Models
+{
+    public class UserSession : Session
+    {
+        public Dictionary<int, List<SessionSeat>> sessionSeats { get; set;}
+
+        private MvcCinemaContext _context { get; set;}
+
+        public UserSession(Session session, MvcCinemaContext context)
+        {
+            _context = context;
+
+            Id = session.Id;
+            HallId = session.HallId;
+            MovieId = session.MovieId;
+            ShowTime = session.ShowTime;
+            Hall = _context.Hall.SingleOrDefault(h => h.Id == HallId);
+            Movie = _context.Movie.SingleOrDefault(m => m.Id == MovieId);
+            sessionSeats = GetFullSessionSeats();
+        }
+
+        private Dictionary<int, List<SessionSeat>> GetFullSessionSeats()
+        {
+            List<SessionSeat> seatsList = new List<SessionSeat>();
+
+            foreach (Seat seat in _context.Seat.ToList())
+            {
+                seatsList.Add(new SessionSeat(seat, this, _context));
+            }
+
+            return GetFormatedSeatList(seatsList);
+
+        }
+
+        private Dictionary<int, List<SessionSeat>> GetFormatedSeatList(List<SessionSeat> unformatted)
+        {
+            List<SessionSeat> listByRowsAndNumber = new List<SessionSeat>();
+
+            listByRowsAndNumber = unformatted.OrderBy(x => x.Row).ThenBy(x => x.Number).ToList();
+
+            var formattedList = GetSeatsByRows(listByRowsAndNumber);
+
+            return formattedList;
+        }
+
+        private Dictionary<int, List<SessionSeat>> GetSeatsByRows(List<SessionSeat> seatsList)
+        {
+            Dictionary<int, List<SessionSeat>> seatsByRow = new Dictionary<int, List<SessionSeat>>();
+            foreach (SessionSeat seat in seatsList)
+            {
+                if (seatsByRow.ContainsKey(seat.Row))
+                {
+                    seatsByRow[seat.Row].Add(seat);
+                }
+                else
+                {
+                    seatsByRow.Add(seat.Row, new List<SessionSeat> { seat });
+                }
+            }
+
+            return seatsByRow;
+        }
+    }
+}
